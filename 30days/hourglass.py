4073a2c46b2801bea265d5e9b04130f6f9ba8509
Matthias <matthiasludwig@me.com>
2020-01-09 11:14:18 +0100

    30days: change initialization of high in hourglass

--- 30days/hourglass.py
+++ 30days/hourglass.py
@@ -6,21 +6,29 @@ import random
 import re
 import sys
 
+def sum_hourglass(arr, hourglass, row, col):
+    sum = 0
+    for i in range(0, len(hourglass[0])):
+        sum += arr[hourglass[0][i] + row][hourglass[1][i] + col]
+
+    return sum
+
 
 
 if __name__ == '__main__':
     arr = []
     hourglass = [[0, 0, 0, 1, 2, 2, 2], [0, 1, 2, 1, 0, 1, 2]]
     sum = 0
-    high = 0
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
+    # Initialize to the first hourglass
+    high = sum_hourglass(arr, hourglass, 0, 0)
+
     for col in range(0, 4):
         for row in range(0, 4):
-            for i in range(0, len(hourglass[0])):
-                sum += arr[hourglass[0][i] + row][hourglass[1][i] + col]
+            sum = sum_hourglass(arr, hourglass, row, col)
 
             if sum > high:
                 high = sum
