871e81733935632b1e4ecc808df91680ac6eeec3
Matthias <matthiasludwig@me.com>
2020-01-09 10:54:17 +0100

    30days: first draft of hourglass finished

--- 30days/hourglass.py
+++ 30days/hourglass.py
@@ -10,9 +10,20 @@ import sys
 
 if __name__ == '__main__':
     arr = []
+    hourglass = [[0, 0, 0, 1, 2, 2, 2], [0, 1, 2, 1, 0, 1, 2]]
+    sum = 0
+    high = 0
 
     for _ in range(6):
         arr.append(list(map(int, input().rstrip().split())))
 
-    for i in range(3):
-        print(i)
+    for col in range(0, 4):
+        for row in range(0, 4):
+            for i in range(0, len(hourglass[0])):
+                sum += arr[hourglass[0][i] + row][hourglass[1][i] + col]
+
+            if sum > high:
+                high = sum
+            sum = 0
+
+    print(high)
