e8c916d4888ed76c4fd8c4ae858ac127e9a2beb2
Matthias <matthiasludwig@me.com>
2020-01-11 16:07:51 +0100

    30days: Implemented abstract class in abstract.py

--- 30days/abstact.py
+++ 30days/abstact.py
@@ -1,23 +1,20 @@
 from abc import ABCMeta, abstractmethod
 class Book(object, metaclass=ABCMeta):
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author   
+    def __init__(self,title,author, price):
+        self.title = title
+        self.author = author
+        self.price = price
     @abstractmethod
     def display(): pass
 
 #Write MyBook class
-class MyBook:
-    def __init__(self, title, author, price):
-        self.title = title
-        self.author = author
-        self.price = price
+class MyBook(Book):
     
     def display(self):
         print("Title: {}\nAuthor: {}\nPrice: {}".format(self.title, self.author, self.price))
 
-title=input()
-author=input()
-price=int(input())
-new_novel=MyBook(title,author,price)
+title = input()
+author = input()
+price = int(input())
+new_novel = MyBook(title,author,price)
 new_novel.display()
