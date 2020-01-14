51b6ce0fc2bbb0c16fd0438531c978f49c1fcd34
Matthias <matthiasludwig@me.com>
2020-01-14 22:16:29 +0100

    30days: WIP linkedlist.py

---
+++ 30days/linkedlist.py
@@ -0,0 +1,32 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data): 
+        #Complete this method
+        T = Node(data)
+
+        if (head != None):
+            head.next = T
+            head.next = None
+
+        print("T created: {} | {}".format(T.data, T.next))
+
+        return T
+
+
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	  
